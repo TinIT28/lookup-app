4349c8e87466274eaa3839b3f1ef67c088df991a
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-05-16 23:10:56 +0700

    Thuc push Home layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.scss";
 import Home from "./components/Home/Home";
 import Footer from "./components/Layout/Footer/Footer";
 import Header from "./components/Layout/Header/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ backgroundColor: "#F3F3F3" }}>
       <Header />
       <Home />
       <Footer />
--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,65 +1,28 @@
-import {
-  HiMenu,
-  HiTranslate,
-  HiOutlineChatAlt2,
-  HiOutlineUsers,
-} from "react-icons/hi";
-import { TfiAnnouncement } from "react-icons/tfi";
-import { HiMagnifyingGlass } from "react-icons/hi2";
+import HeaderPage from "../../../images/HeaderPage.png";
 import "./Header.css";
+import { HiTranslate } from "react-icons/hi";
 
 function Header() {
   return (
     <div>
-      <div className="Header">
-        <div className="Header__item">
-          <div className="Lookup">
-            {" "}
-            <a className="LookupLink" href="">
-              LookUp.Com
-            </a>
-            <p className="category">
-              {" "}
-              <HiMenu />
-              category
-            </p>
-          </div>
-          <div className="iconHeader">
-            <HiTranslate className="languageIcon" />
-            <HiOutlineChatAlt2 className="chatIcon" />
-            <TfiAnnouncement className="speakerIcon" />
-            <HiOutlineUsers className="userIcon" />
-            <a href="">Suport</a>{" "}
-          </div>
+      <div className="header_top">
+        <a className="header_lookup" href="">
+          LookUp.com
+        </a>
+        <div>
+          {" "}
+          <HiTranslate />
+          <a className="header_help" href="">
+            Trợ giúp
+          </a>
         </div>
-        <div className="Header__body">
-          <div className="Header__body__item">
-            <a className="Header__item__body" href="">
-              New Feed
-            </a>
-            <a className="Header__item__body" href="">
-              Product
-            </a>
-            <a className="Header__item__body" href="">
-              Producer
-            </a>
-          </div>
-          <div className="Search__header">
-            <input
-              className="inputHeader"
-              type="text"
-              placeholder="Product..."
-              name=""
-              id=""
-            />
-            <div>
-              <button className="SearchIcon">
-                <HiMagnifyingGlass style={{ width: "30px", height: "20px" }} />
-                <h3 style={{ color: "white" }}>Search</h3>
-              </button>
-            </div>
-          </div>
-        </div>
+      </div>
+      <div className="header_body">
+        <img className="header_img" src={HeaderPage} alt="" />
+        <h3 className="header_title">
+          Khám phá và kết nối với các doanh nghiệp <br /> địa phương trong khu
+          vực của bạn
+        </h3>
       </div>
     </div>
   );
