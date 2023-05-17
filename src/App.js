5696371835455a71c8394bc522298215ce28e816
Macbook Pro <macbookpro@Macbooks-MBP.lan>
2023-05-17 07:32:11 +0700

    upload route-dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import "./App.scss";
 import "./login.scss";
 import Header from "./components/Layout/Header/Header";
 import LoginForm from "./components/User/LoginForm";
+import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import NotFound from "./components/Layout/Error/NotFound";
+import { HomePage } from "./components/User/HomePage";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/login" element={<LoginForm></LoginForm>}/>
+          <Route path="/*" element={<NotFound/>}/>
+        </Routes>
+      </Router>  
     </div>
   );
 }
