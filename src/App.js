797bb8f4ed930bd7b0a45f97c5d3e8267ed50608
TinIT28 <tinmcit@gmail.com>
2023-05-17 11:45:37 +0700

    Fix bug package.json and App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.scss";
 import Header from "./components/Layout/Header/Header";
+import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import NotFound from "./components/Layout/Error/NotFound";
+import Footer from './components/Layout/Footer/Footer';
+import { HomePage } from './components/User/HomePage';
+import LoginForm from './components/User/LoginForm';
 
 function App() {
   return (
@@ -9,7 +14,7 @@ function App() {
         <Footer />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginForm></LoginForm>} />
+          <Route path="/login" element={<LoginForm />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </Router>
