341255d8cac7e631821cf4c5fa707e13c517b6ee
Macbook Pro <macbookpro@Macbooks-MBP-2.lan>
2023-05-21 21:44:55 +0700

    update home page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import "./App.scss";
+import './index.css';
 import Header from "./components/Layout/Header/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./components/Layout/Error/NotFound";
-import Footer from "./components/Layout/Footer/Footer";
-import Home from "./components/Home/Home";
 import LoginForm from "./components/User/LoginForm";
 import { Fragment } from "react";
-import NewFeed from "./components/Home/NewFeed";
+import Navbar from './components/Home/Navbar';
+import Hero from './components/Home/Hero';
+import About from './components/Home/About';
+import Tours from './components/Home/Tours';
+import Footer from './components/Home/Footer';
+import Services from './components/Home/Services';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Fragment>
-          <Header />
-          <Routes>
-            <Route path="/" element={<NewFeed />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-          <Footer />
-        </Fragment>
-      </Router>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Home/Footer.js
@@ -0,0 +1,48 @@
+import { socialLinks } from './data';
+import { pageLinks } from './data';
+import PageLinks from './PageLinks';
+import SocialLink from './SocialLink';
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      {/* <PageLinks parentClass='footer-links' itemClass='footer-link' /> */}
+      <ul className="footer-links">
+        {pageLinks.map((link) => {
+          const { id, href, text } = link;
+          return (
+            <SocialLink key={link.id}{...link} itemClass="footer-icon"/>
+            // <li key={id}>
+            //   <a href={href} 
+            //   className="footer-link">
+            //     {text}
+            //   </a>
+            // </li>
+          );
+        })}
+      </ul>
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                className="footer-icon"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
